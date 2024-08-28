f8c44ecd54ab518464db523927c9dcbe81a2f1b1
Asharafraza Desai <asharafrazadesai2002@gmail.com>
2024-08-28 11:15:30 +0530

    Intial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,18 @@ import Home from './components/Home';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Navbar from './components/shared/Navbar';
+import Jobs from './components/Jobs';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/jobs" element={<Jobs/>} />
+
       </Routes>
     </Router>
   );
